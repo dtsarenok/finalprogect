b78d804b3ce95618ed8ae134a38eb96202952d77
dtsarenok <147497766+dtsarenok@users.noreply.github.com>
2025-05-12 00:15:35 +0900

    Update models.go

--- internal/models/models.go
+++ internal/models/models.go
@@ -2,16 +2,15 @@ package models
 
 // User представляет пользователя системы
 type User struct {
-	ID           int64  // Уникальный идентификатор пользователя
-	Login        string // Логин пользователя
-	PasswordHash string // Хеш пароля пользователя
+	ID           int64  
+	Login        string
+	PasswordHash string
 }
 
-// Calculation представляет вычисление пользователя
 type Calculation struct {
-	ID         int64  // Уникальный идентификатор вычисления
-	UserID     int64  // Идентификатор пользователя, которому принадлежит вычисление
-	Expression string // Выражение для вычисления
-	Result     string // Результат вычисления
-	CreatedAt  string // Время создания (формат можно уточнить)
+	ID         int64 
+	UserID     int64
+	Expression string
+	Result     string
+	CreatedAt  string
 }
